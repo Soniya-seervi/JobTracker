8a2734816c075c280dcd72ebcad6fb49bb70be67
Soniya Seervi <soniyaseervi12@gmail.com>
2023-04-23 13:40:05 +0530

    Test User

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,8 @@ const auth = async(req, res, next) => {
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         // attaching the user request object
-        req.user = {userId: payload.userId}
+        const testUser = payload.userId === '6444e1a2007d0c717eecfbd5'
+        req.user = {userId: payload.userId, testUser}
         next()
     } catch (error) {
         throw new UnAuthenticatedError('Authentication invalid, token not verified')
