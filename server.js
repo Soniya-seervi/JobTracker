4685c9ffd50352e7d8c6397c7b7b99257b33e602
Soniya Seervi <soniyaseervi12@gmail.com>
2023-04-23 13:06:23 +0530

    Security Pages

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ dotenv.config()
 import 'express-async-errors'
 import morgan from 'morgan'
 
+import {dirname} from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
 // db and authenticate user
 import connectDB from './db/connect.js'
 
@@ -17,24 +25,26 @@ import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import authenticateUser from './middleware/auth.js'
 
-import { log } from 'console'
 
 if(process.env.NODE_ENV !== 'production'){
     app.use(morgan('dev'))
 }
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 
-app.get('/', (req, res) => {
-    res.json({msg: "Welcome"})
-})
-
-app.get('/api/v1', (req, res) => {
-    res.json({msg: "API"})
-})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
